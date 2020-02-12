e5d0417800fe5db77344ca7cdd8bc31a985e4cc0
w77996 <912513404@qq.com>
2020-02-12 23:33:16 +0800

    爬虫定时器编写

--- HiTop-Crawler/excute.py
+++ HiTop-Crawler/excute.py
@@ -1,1 +1,22 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
+from crawler import CrawlHotData
+import asyncio
+
+
+def excute_crawler():
+    print('4444')
+    crawl_hot_data = CrawlHotData()
+    # loop = asyncio.get_event_loop()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    task = [crawl_hot_data.get_github_hot('https://github.com/trending')]
+    # # CrawlHotData.get_weibo_host(self, 'https://s.weibo.com/top/summary')
+    loop.run_until_complete(asyncio.gather(*task))
+    loop.close()
+
+
+if __name__ == '__main__':
+    scheduler = BlockingScheduler()
+
+    scheduler.add_job(excute_crawler, 'cron', hour=23, minute=33)
+    scheduler.start()
