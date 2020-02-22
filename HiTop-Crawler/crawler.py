3d9b6a874dd4b89889a70d14506b6d562ca82c62
w77996 <912513404@qq.com>
2020-02-22 16:20:42 +0800

    请求ssl修改

--- HiTop-Crawler/crawler.py
+++ HiTop-Crawler/crawler.py
@@ -20,7 +20,8 @@ class CrawlHotData:
 
     # 爬取微博热门
     async def get_weibo_hot(self, url):
-        async with aiohttp.ClientSession(headers=self.headers) as session:
+        conn = aiohttp.TCPConnector(verify_ssl=False)  # 防止ssl报错
+        async with aiohttp.ClientSession(headers=self.headers, connector=conn) as session:
             async with session.get(url) as response:
                 if response.status == 200:
                     result_html = etree.HTML(await response.text())
@@ -45,7 +46,8 @@ class CrawlHotData:
     async def get_zhihu_hot(self, url):
         self.headers[
             'cookie'] = '_zap=d3b85524-608a-40a0-a0c8-6cb65d433f06; d_c0="ACAhXCO-BRCPTl_RLKNCOS6yneaLpF29q4E=|1568043294"; tgw_l7_route=f2979fdd289e2265b2f12e4f4a478330; _xsrf=cbc8c703-c14e-4a2f-8e4d-3d80c4da82da; capsion_ticket="2|1:0|10:1568095339|14:capsion_ticket|44:Zjg4ZmRjMmY2ZTJjNDVkNTgwMWNiNjA1NjU5NTYzMzA=|506899d0030a455c56324b2bdf2592270dce960f4187dd6111662ee61824fb12"; l_n_c=1; r_cap_id="MmM4NjBkNTI0YTc4NGFmNGFiM2FjYzUwY2RkMmE0ZmQ=|1568095343|54d1cfb6dc5004e521366794171e1b01c584ef6b"; cap_id="MTI2MTIwNThhNWRlNGI1YzhjMzU5ZDEzZTQxZTJiMzg=|1568095343|2d87297fbb65212e7c20c6de9b35e64bce784945"; l_cap_id="ODI1N2Y0YjVhMDViNDI1Mjk2ZTkyNTY1M2RlMGYxZTQ=|1568095343|82490ce4fb492327ee04ba45315ec8906b136728"; n_c=1; z_c0=Mi4xQ09MUkNRQUFBQUFBSUNGY0k3NEZFQmNBQUFCaEFsVk5lb3BrWGdENlJUT1Vwc3BIdkgzSVhXN0pGUExPVlRvZU1R|1568095354|fc1d394d3725d0934fc9fd6b606932e1bb7ad659; tshl=; tst=h; unlock_ticket="AJBihgA5qw0XAAAAYQJVTYVDd10T-ZK-VwMEje_V-X8D4kfV2fn6TA=="'
-        async with aiohttp.ClientSession(headers=self.headers) as session:
+        conn = aiohttp.TCPConnector(verify_ssl=False)  # 防止ssl报错
+        async with aiohttp.ClientSession(headers=self.headers, connector=conn) as session:
             async with session.get(url) as response:
                 if response.status == 200:
                     result_html = etree.HTML(await response.text())
@@ -69,7 +71,8 @@ class CrawlHotData:
 
     # 爬取Github热门
     async def get_github_hot(self, url):
-        async with aiohttp.ClientSession(headers=self.headers) as session:
+        conn = aiohttp.TCPConnector(verify_ssl=False)  # 防止ssl报错
+        async with aiohttp.ClientSession(headers=self.headers,connector=conn) as session:
             async with session.get(url) as response:
                 if response.status == 200:
                     result_html = etree.HTML(await response.text())
